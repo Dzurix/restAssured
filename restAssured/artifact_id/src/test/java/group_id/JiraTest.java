2a72f76ae54fd30472b03194505d797ce778fdca
Dexter <deki2ns@gmail.com>
2023-01-11 06:02:34 +0100

    oAuth

--- restAssured/artifact_id/src/test/java/group_id/JiraTest.java
+++ restAssured/artifact_id/src/test/java/group_id/JiraTest.java
@@ -4,8 +4,9 @@ import static io.restassured.RestAssured.*;
 
 import io.restassured.RestAssured;
 import io.restassured.filter.session.SessionFilter;
+import io.restassured.path.json.JsonPath;
 import java.io.File;
-import javax.print.DocFlavor.STRING;
+import org.testng.Assert;
 
 public class JiraTest {
 
@@ -23,6 +24,7 @@ public class JiraTest {
     //  II nacin je ovde primenjen preko   SessionFilter-a  =>  slusa kada se kreira nova sesija i hvata podatke
 
     String response = given()
+      .relaxedHTTPSValidation() // OVO JE DA ZAOBIDJEMO HTTPS validaciju ->  uvek to dodajem
       .header("Content-Type", "application/json")
       .pathParam("Key", "10101")
       .log()
@@ -46,13 +48,17 @@ public class JiraTest {
     // 2. Adding a coment to existing issue in JIRA    using   ADD COMMENT API
     // a da bi do mogao, treba mi session ID, zato koristim FILTER SESSION
 
-    given()
+    String expectedMessage = "Hey, how are you?";
+
+    String addCommentResponse = given()
       .pathParam("Key", "10101")
       .log()
       .all()
       .header("Content-Type", "application/json")
       .body(
-        "{\r\n        \"body\": \"This is mine first comment\",\r\n   " +
+        "{\r\n        \"body\": \"" +
+        expectedMessage +
+        "\",\r\n   " +
         "     \"visibility\": {\r\n            \"type\": \"role\",\r\n     " +
         "       \"value\": \"Administrators\"\r\n        }\r\n    }"
       )
@@ -63,7 +69,14 @@ public class JiraTest {
       .log()
       .all()
       .assertThat()
-      .statusCode(201);
+      .statusCode(201)
+      .extract()
+      .response()
+      .asString(); //ovde extraktujem response da bi ga mogao izvuci ID od komentara koji mi treba u tacki 5.
+
+    JsonPath js = new JsonPath(addCommentResponse);
+    String commentID = js.getString("id");
+
     // 3. Add attachment to existing issue using ADD ATTACHMENT
 
     // primer kako se salje atachment u JIRI   =>            curl -D- -u admin:admin -X POST -H "X-Atlassian-Token: no-check" -F "file=@myfile.txt" http://myhost/rest/api/2/issue/TEST-123/attachments
@@ -96,5 +109,28 @@ public class JiraTest {
       .extract()
       .response()
       .asString();
+    //5. Complex Jira Json response to retrieve the added Comment with code logic
+
+    // sada proveravam ID od komentara koje sam dobio iz responsa is tacke 4.
+
+    JsonPath js1 = new JsonPath(issueDetails);
+    int commentsCounts = js1.get("fields.comment.comments.size()");
+
+    for (int i = 0; i < commentsCounts; i++) {
+      String commentIdIssue = js1
+        .get("fields.comment.comments[" + i + "].id")
+        .toString();
+
+      if (commentIdIssue.equalsIgnoreCase(commentID)) {
+        String message = js1
+          .get("fields.comment.comments[" + i + "].body")
+          .toString();
+
+        System.out.println(message);
+
+        //TEST NG ASSERTION
+        Assert.assertEquals(message, expectedMessage);
+      }
+    }
   }
 }
